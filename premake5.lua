7045cc555c05e80ed91933ef42808d898321e65d
kubgus <ja.gustafik@outlook.sk>
2024-02-14 17:22:42 +0100

    Add premake.lua

---
+++ premake5.lua
@@ -0,0 +1,57 @@
+workspace "texemble"
+    configurations { "debug", "release" }
+    architecture "x86_64"
+
+    filter "configurations:debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:release"
+        defines { "RELEASE" }
+        optimize "Speed"
+        flags { "LinkTimeOptimization" }
+
+outdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "texemble"
+    location "src/texemble"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outdir .. "/%{prj.name}")
+    objdir ("obj/" .. outdir .. "/%{prj.name}")
+
+    files {
+        "src/%{prj.name}/**.cpp",
+        "src/%{prj.name}/**.h",
+    }
+
+    includedirs {
+        "src/%{prj.name}",
+    }
+
+project "sandbox"
+    location "src/sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outdir .. "/%{prj.name}")
+    objdir ("obj/" .. outdir .. "/%{prj.name}")
+
+    files {
+        "src/%{prj.name}/**.cpp",
+        "src/%{prj.name}/**.h",
+    }
+
+    includedirs {
+        "src/%{prj.name}",
+        "src/texemble",
+    }
+
+    links {
+        "texemble",
+    }
