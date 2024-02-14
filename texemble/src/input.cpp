b3507ec7cd987b01793693dce2be6b4d225764f8
kubgus <ja.gustafik@outlook.sk>
2024-02-15 00:26:29 +0100

    Add Windows support for input

--- texemble/src/input.cpp
+++ texemble/src/input.cpp
@@ -3,11 +3,13 @@
 namespace txm {
 
     input::input() {
+#ifndef TXM_WINDOWS
         tcgetattr(STDIN_FILENO, &_st);
         _nd = _st;
 
         _nd.c_lflag &= ~(ICANON | ECHO);
         tcsetattr(STDIN_FILENO, TCSANOW, &_nd);
+#endif
     }
 
     input::~input() {
@@ -20,8 +22,12 @@ namespace txm {
     void input::handle(std::function<void(char latest)> handle) {
         _handler = std::thread([&]() {
             while (true) {
-                char input = '\0';
+            char input = '\0';
+#ifndef TXM_WINDOWS
                 read(STDIN_FILENO, &input, 1);
+#else
+                if (_kbhit()) input = _getch();
+#endif
                 handle(input);
             }
         });
--- texemble/src/input.h
+++ texemble/src/input.h
@@ -2,8 +2,13 @@
 
 #include <functional>
 #include <thread>
+
+#ifndef TXM_WINDOWS
 #include <termios.h>
 #include <unistd.h>
+#else
+#include <conio.h>
+#endif
 
 namespace txm {
 
